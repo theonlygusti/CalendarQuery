79398d3c08cbfcb05f13a22ac5a00d19a90741d5
BuildTools <theonlygusti@gmail.com>
2016-12-26 11:59:51 +0000

    Type checking constructor arguments for CalendarEvent

--- plugin.py
+++ plugin.py
@@ -6,11 +6,26 @@ import unittest
 
 class CalendarEvent:
     """Represents a macOS calendar event."""
+    __allowed_properties = {
+            "name": str,
+            "location": str,
+            "starts": datetime.datetime,
+            "ends": datetime.datetime,
+            "repeat": datetime.timedelta,
+            "end_repeat": datetime.datetime,
+            "travel_time": datetime.timedelta,
+            "alert": datetime.timedelta,
+            "notes": str,
+            "invitees": list
+            }
+
     def __init__(self, **keyword_arguments):
-        self.__allowed_properties = ["starts", "ends", "repeat", "end_repeat", "travel_time", "alert", "notes", "invitees"]
-        for key, value in keyword_arguments:
+        for key, value in keyword_arguments.items():
             if key in self.__allowed_properties:
-                setattr(self, key, value)
+                if type(value) is self.__allowed_properties[key]:
+                    setattr(self, key, value)
+                else:
+                    raise TypeError("%s must be %s" % (key, str(self.__allowed_properties[key])))
         
 def find_time_of_event(event):
     """Return a CalendarEvent with properties associated with the unparsed user-inputted string event."""
@@ -45,3 +60,17 @@ def results(fields, original_query):
 
 def run(query):
     os.system("open -a Calendar")
+
+class TestClasses(unittest.TestCase):
+    """Test that the classes defined by this script function correctly."""
+
+    def test_CalendarEvent(self):
+        party_tomorrow = CalendarEvent(
+                name = "After Prom",
+                location = "The big barn",
+                starts = datetime.datetime.today() + datetime.timedelta(days = 1),
+                ends = datetime.datetime.today() + datetime.timedelta(days = 1, hours = 5),
+                travel_time = datetime.timedelta(hours = 1),
+                notes = "This is gonna be a load of fun."
+                )
+
